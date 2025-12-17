99dfe62b29e853741814743d814b79e7289113f1
Arafatul Islam <56130767+Arafatislm@users.noreply.github.com>
2025-12-18 03:33:07 +0600

    Update weather-dashboard.php

--- weather-dashboard.php
+++ weather-dashboard.php
@@ -3,7 +3,7 @@
  * Plugin Name: Weather Dashboard
  * Plugin URI: https://github.com/arafatislm/weather-dashboard
  * Description: Displays weather for specific cities using Open-Meteo API. Use [weather_dashboard].
- * Version: 3.3.0
+ * Version: 3.4.0
  * Author: Arafatul Islam
  * Author URI: https://arafatislam.net
  * License: GPL v2 or later
